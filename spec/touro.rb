a1af059660296a4d84c8bb2a546bac258764b5d5
claudioveiga <claudio@veiga.net.br>
2021-03-05 19:50:55 -0300

    Correção cor touro

--- spec/touro.rb
+++ spec/touro.rb
@@ -3,7 +3,7 @@ require_relative "./animal.rb"
 class Touro < Animal
 
     attr_accessor :habitatCaseiro, :atacaHumanos, :alimentacao, :somQueFaz, :cor, :idade
-    def initialize(idade)
+    def initialize(cor, idade)
         @habitatCaseiro = false
         @atacaHumanos = true
         @alimentacao = "Capim"
